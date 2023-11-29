8c2a2a3c75363eee7e08919694cc87d2589de7e6
venkatparvatini <venkat.parvatini@photon.com>
2023-11-29 11:11:51 +0530

    Deleted 2 files

--- src/test/java/extentreports1/test4.java
+++
@@ -1,8 +0,0 @@
-package extentreports1;
-
-public class test4 {
-	public static void main(String[] args) {
-		System.out.println("TEst4");
-	}
-
-}
--- src/test/java/extentreports1/test5.java
+++
@@ -1,9 +0,0 @@
-package extentreports1;
-
-public class test5 {
-	
-	public static void main(String[] args) {
-		System.out.println("Test5");
-	}
-
-}
